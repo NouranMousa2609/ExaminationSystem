366cb78ed6ae51787441344603ca9c0fe9036940
NouranMousa2609 <nouranmousa55@gmail.com>
2024-07-20 13:37:06 +0300

    1. Design a Class to represent the Question Object, Question Question is consisting of: a. Header of the question b. Body of the question c. Mark

---
+++ ExaminationSystem/Question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem
+{
+
+   abstract class Question
+   {
+
+       public string? QuestionHeader { get; set; }
+
+       public string? QuestionBody { get; set; }
+
+       public Double Mark { get; set; }
+
+
+       protected Question()
+       {
+
+       }
+       protected Question(string questionHeader, string questionBody, double mark)
+       {
+           QuestionHeader = questionHeader;
+           QuestionBody = questionBody;
+           Mark = mark;
+
+       }
+   }
+ 
+}
